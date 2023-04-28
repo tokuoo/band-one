a36b00d7d9cbbacef6f94c8704886e2aa03fc8bf
tokuoo <tokuoo4242@gmail.com>
2023-04-28 16:34:26 +0900

    ペジネーション

---
+++ database/migrations/2023_04_21_173227_create_posts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable();
+            $table->string('title', 50);
+            $table->string('body', 400);
+            $table->string('place',);
+            $table->integer('recruit_nummbers');
+            $table->string('recruit_part');
+            $table->string('genre');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
